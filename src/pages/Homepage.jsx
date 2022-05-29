75f198b4f7a215548bea1972ee32965d3de85320
vladimirSEOadv <vladimirSEOadv@gmail>
2022-05-29 23:22:31 +0300

    start commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Homepage</h1>
+            <p>My home page</p>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0qkucBvbQYl8t_ZKkJVkFdbPx-_66BwrgIoMKCrvsroPsAuNUSbNf6_-hX5s4m2fUbSE&usqp=CAU" alt=""/>
+        </div>
+    );
+};
+
+export default Homepage;
